fc4ef0b3dce8de891370e2da07f68c627ada8035
Terebent Roxana <roxana.terebent@stud.ubbcluj.ro>
2025-05-21 00:36:31 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const BASE_IP = import.meta.env.VITE_BASE_IP || 'localhost';
-const BASE_URL = import.meta.env.VITE_API_URL || `http://${BASE_IP}:5000`;
+const BASE_IP = import.meta.env.VITE_BASE_IP 
+const BASE_URL = import.meta.env.VITE_API_URL 
 
 export { BASE_IP, BASE_URL };
