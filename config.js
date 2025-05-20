13a4e70d2baddf713a75e89a87cbe0fc5f24ab29
Terebent Roxana <roxana.terebent@stud.ubbcluj.ro>
2025-05-21 01:47:49 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const BASE_IP = import.meta.env.VITE_BASE_IP 
-const BASE_URL = import.meta.env.VITE_API_URL 
+const BASE_IP = 'localhost';
+const BASE_URL ="https://harmonicarchive-backend-production.up.railway.app";
 
 export { BASE_IP, BASE_URL };
