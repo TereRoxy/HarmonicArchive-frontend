f78ffffe6ad9f705cea4e33fd8dbd1a5cc84a355
Terebent Roxana <roxana.terebent@stud.ubbcluj.ro>
2025-06-17 00:15:50 +0300

    added gtag for analytics

--- src/main.js
+++ src/main.js
@@ -2,8 +2,20 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueGtag from "vue-gtag-next"
 
 const app = createApp(App);
 import { faker } from "@faker-js/faker";
 
-app.use(router).mount("#app");
+// Use the router
+app.use(router);
+
+// Use VueGtag for Google Analytics
+app.use(VueGtag, {
+    property: {
+      id: "G-0G20GXPF97", // Replace with your Google Analytics Measurement ID
+    },
+  });
+
+// Mount the app
+app.mount("#app");
